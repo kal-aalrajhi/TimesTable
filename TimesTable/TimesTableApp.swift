96a7c6759a3fff87846df9efd779a52f0e1ebec2
Kal <kal.aalrajhi@gmail.com>
2023-06-20 09:57:40 -0400

    Initial Commit

---
+++ TimesTable/TimesTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTableApp.swift
+//  TimesTable
+//
+//  Created by Dr Cpt Blackbeard on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct TimesTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
